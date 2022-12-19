20cc8331786f0c1d9a1ed8fefaf4dbb57e709fdc
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-19 07:59:54 +0530

    [MAhesh]Add:N Log Folder Structure is created

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Calculation;
+using System;
+namespace Calculations
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*****Well Come To NLog Project:******");
+            Sum add = new Sum();
+            add.DoSum(5, 6);
+        }
+    }
+}
---
+++ Sum.cs
@@ -0,0 +1,27 @@
+﻿using NLog.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    internal class Sum
+    {
+         NLog nlog = new NLog();
+        public void DoSum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                nlog.LogError("a and b values are zero");
+            }
+            else
+            {
+                int c = a + b;
+                nlog.LogDebug("The Program excecuting");
+                nlog.LogInfo("values adding");
+            }
+        }
+    }
+}
